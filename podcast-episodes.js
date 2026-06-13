5847f35990f8114fcb77551c381634417eca9ca0
Fatih <fatihb@Fatih-MacBook-Air.local>
2026-06-13 23:00:15 +0300

    fix: podcast bolumleri icin fallback veri ekle

---
+++ podcast-episodes.js
@@ -0,0 +1,86 @@
+window.PODCAST_FALLBACK_EPISODES = [
+  {
+    title: "Kurumsal Yapay Zeka Pazarının Genel Durumu: Yapay Zeka Yatırımları Ne Durumda?",
+    desc: "Otostopçunun Yapay Zeka Rehberi'nin bu bölümünde yapay zeka yatırımlarının deneysel aşamalardan çıkıp endüstriyel ölçek...",
+    date: "6 Haz 2026",
+    link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Kurumsal-Yapay-Zeka-Pazarnn-Genel-Durumu-Yapay-Zeka-Yatrmlar-Ne-Durumda-e3ke4bk",
+    image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1780773679291-118860f08ce83.jpg"
+  },
+  {
+    title: "Çin Yapay Zeka'da Nasıl Süper Güç Oldu: Çin'in ve Çinli Yenilikçilerin Öğrettikleri",
+    desc: "Otostopçunun Yapay Zeka Rehberi'nde, yapay zeka dünyasında Batı'nın hegemonyasını temelden sarsan destansı bir teknoloj...",
+    date: "3 May 2026",
+    link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/in-Yapay-Zekada-Nasl-Sper-G-Oldu-inin-ve-inli-Yenilikilerin-rettikleri-e3iqvua",
+    image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1777837876948-2726b92f8b74d.jpg"
+  },
+  {
+    title: "TeknoFeodalizm, Palantir'in Teknoloji Cumhuriyeti Manifestosu, Sam Altman ve Yapay Zeka ile Gelecek Bolluk",
+    desc: "Bu hafta Otostopçunun Yapay Zeka Rehberi'nde son dönemlerde yaşadığımız, gördüğümüz okuduğumuz gelişmelerle, Yanis Varou...",
+    date: "20 Nis 2026",
+    link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/TeknoFeodalizm--Palantirin-Teknoloji-Cumhuriyeti-Manifestosu--Sam-Altman-ve-Yapay-Zeka-ile-Gelecek-Bolluk-e3i6lr6",
+    image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1776714327083-8e618e6fd4d19.jpg"
+  },
+  {
+    title: "Yapay Zeka, Girişimcilik, Kurumsal Dönüşüm: Nisa Akgün - Dijital İş Geliştirme Yöneticisi / Zorlu Holding",
+    desc: "Otostopçunun Yapay Zeka Rehberi'nde bu bölüm girişimcilik, yatırımcılık, inovasyon ekosistemi tecrübesini kurumsal yapa...",
+    date: "10 Nis 2026",
+    link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zeka--Giriimcilik--Kurumsal-Dnm-Nisa-Akgn---Dijital--Gelitirme-Yneticisi--Zorlu-Holding-e3hn3ut",
+    image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1775827712725-e7b168003950f.jpg"
+  },
+  {
+    title: "Yapay Zeka, Bankacılık ve Finans - Konuk: İlker Kuruöz - Garanti BBVA Genel Müdür Yardımcısı",
+    desc: "Otostopçunun Yapay Zeka Rehberi'nde bu bölümümüzde, finansın teknolojiyle kırılma noktasını keşfetmek için Garanti BBVA...",
+    date: "28 Mar 2026",
+    link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zeka--Bankaclk-ve-Finans---Konuk-lker-Kuruz---Garanti-BBVA-Genel-Mdr-Yardmcs-e3h38o5",
+    image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1774706723835-d74f740d148aa.jpg"
+  },
+  {
+    title: "Yapay Zeka Kansere Çare Bulabilir Mi? Yapay Zeka ve Biyoteknoloji",
+    desc: "İnsanlık, kendini yeniden yazma gücüne kavuştuğunda ne yapacak? Otostopçunun Yapay Zeka Rehberi'nde bu bölümde DNA'nın k...",
+    date: "16 Mar 2026",
+    link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zeka-Kansere-are-Bulabilir-Mi--Yapay-Zeka-ve-Biyoteknoloji-e3ghkju",
+    image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1773686943601-c055f638b92d7.jpg"
+  },
+  {
+    title: "Yapay Zekanın Etik, Güvenli Şirketi, Claude'ın Yaratıcısı: Anthropic ve Pentagon Krizi",
+    desc: "Otostopçunun Yapay Zeka Rehberi'nin bu bölümünde, yapay zeka dünyasının en sıra dışı şirketlerinden biri olan Anthropic'...",
+    date: "8 Mar 2026",
+    link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zekann-Etik--Gvenli-irketi--Clauden-Yaratcs-Anthropic-ve-Pentagon-Krizi-e3g3tgq",
+    image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1772962723468-5e6191b8da4a9.jpg"
+  },
+  {
+    title: "Yapay Zeka, Savunma Sanayii, İstihbarat: Herşeyin Palantirleşmesi",
+    desc: "Otostopçunun Yapay Zeka Rehberi'nde bu hafta The Philosopher in the Valley: Alex Karp, Palantir and the Rise of the Sur...",
+    date: "24 Şub 2026",
+    link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zeka--Savunma-Sanayii--stihbarat-Hereyin-Palantirlemesi-e3fiaiq",
+    image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1771965660640-e1828763cc176.jpg"
+  },
+  {
+    title: "50. Bölüm - Açıklanabilir Yapay Zeka (Explainable AI - XAI)",
+    desc: "Otostopçunun Yapay Zeka Rehberi'nin bu bölümünde, yapay zekanın \"kara kutu\" gizemini aralayarak algoritmik kararları...",
+    date: "8 Şub 2026",
+    link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/50--Blm---Aklanabilir-Yapay-Zeka-Explainable-AI---XAI-e3eptdi",
+    image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1770542386100-4e9094aad49d2.jpg"
+  },
+  {
+    title: "Teknoloji Dünyasının 2026 Davos ve Yapay Zeka Notları - Yapay Zekanın İşgücü ve Ekonomi Etkisi",
+    desc: "Otostopçunun Yapay Zeka Rehberi'nde bu hafta teknoloji dünyasının yapay zeka odaklı DAVOS notlarına bakacağız beraber...",
+    date: "1 Şub 2026",
+    link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Teknoloji-Dnyasnn-2026-Davos-ve-Yapay-Zeka-Notlar---Yapay-Zekann-gc-ve-Ekonomi-Etkisi-e3eg3v0",
+    image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1769970583544-d466bcab6cd72.jpg"
+  },
+  {
+    title: "Yapay Zeka Araştırmaları Bizi Bu Günlere Nasıl Getirdi: Yeni Araştırma Çağı İhtiyacı",
+    desc: "Otostopçunun Yapay Zeka Rehberi'nde bu bölüm, 2000'ler sonrası yapay zekada yaşanan büyük sıçramayı, derin öğrenme...",
+    date: "22 Oca 2026",
+    link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zeka-Aratrmalar-Bizi-Bu-Gnlere-Nasl-Getirdi-Yeni-Aratrma-a-htiyac-e3e1oij",
+    image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1769104868528-85f525fc81c03.jpg"
+  },
+  {
+    title: "Yapay Zeka Çağının Süper Yeteneği: High Agency (Yüksek Eylemlilik) Nedir? Nasıl Kazanılır?",
+    desc: "Otostopçunun Yapay Zeka Rehberi'nde bu bölüm High Agency yani yüksek eylemlilik kavramını derinlemesine ele alıyoruz...",
+    date: "17 Oca 2026",
+    link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zeka-ann-Sper-Yetenei-High-Agency-Yksek-Eylemlilik-Nedir--Nasl-Kazanlr-e3dpejd",
+    image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1768642113453-067ebf047b98.jpg"
+  }
+];
