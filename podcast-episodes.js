06072b4dd2b6b0091db31f375990cfe142af25b8
Fatih <fatihb@10.5.88.75>
2026-07-07 20:02:40 +0300

    Podcast: per-episode platform links, auto-refresh, UI buttons; add sose.jpeg and academic updates

--- podcast-episodes.js
+++ podcast-episodes.js
@@ -4,6 +4,9 @@ window.PODCAST_FALLBACK_EPISODES = [
     desc: "Otostopçunun Yapay Zeka Rehberi'nde bu bölümde yapay zekanın, blokzincirin ve tokenizasyonun kesiştiği yeni nesil makine ekonomisini konuşuyoruz.",
     date: "30 Haz 2026",
     link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zeka-ve-Token-Ekonomisi--Yeni-Yapay-Zeka-ve-Blockhain-Ekonomisi-Neye-Benziyor-e3lgaoh",
+    platformLinks: {
+      spotify: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zeka-ve-Token-Ekonomisi--Yeni-Yapay-Zeka-ve-Blockhain-Ekonomisi-Neye-Benziyor-e3lgaoh"
+    },
     image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1782859960011-0317c91ca59d3.jpg"
   },
   {
@@ -11,6 +14,9 @@ window.PODCAST_FALLBACK_EPISODES = [
     desc: "Otostopçunun Yapay Zeka Rehberi'nde bu bölümde yapay zekanın iklim sürdürülebilirliğiyle ilişkisini; akıllı şebekelerden hassas tarıma uzanan çözüm alanlarını ele alıyoruz.",
     date: "16 Haz 2026",
     link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zeka-Destekli-klim-zmleri-Yapay-Zeka-klim-Sorununa-are-Olur-Mu-e3ksljv",
+    platformLinks: {
+      spotify: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zeka-Destekli-klim-zmleri-Yapay-Zeka-klim-Sorununa-are-Olur-Mu-e3ksljv"
+    },
     image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1781639443036-c304c7b37e76c.jpg"
   },
   {
@@ -18,6 +24,9 @@ window.PODCAST_FALLBACK_EPISODES = [
     desc: "Otostopçunun Yapay Zeka Rehberi'nin bu bölümünde yapay zeka yatırımlarının deneysel aşamalardan çıkıp endüstriyel ölçek...",
     date: "6 Haz 2026",
     link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Kurumsal-Yapay-Zeka-Pazarnn-Genel-Durumu-Yapay-Zeka-Yatrmlar-Ne-Durumda-e3ke4bk",
+    platformLinks: {
+      spotify: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Kurumsal-Yapay-Zeka-Pazarnn-Genel-Durumu-Yapay-Zeka-Yatrmlar-Ne-Durumda-e3ke4bk"
+    },
     image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1780773679291-118860f08ce83.jpg"
   },
   {
@@ -25,6 +34,9 @@ window.PODCAST_FALLBACK_EPISODES = [
     desc: "Otostopçunun Yapay Zeka Rehberi'nde, yapay zeka dünyasında Batı'nın hegemonyasını temelden sarsan destansı bir teknoloj...",
     date: "3 May 2026",
     link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/in-Yapay-Zekada-Nasl-Sper-G-Oldu-inin-ve-inli-Yenilikilerin-rettikleri-e3iqvua",
+    platformLinks: {
+      spotify: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/in-Yapay-Zekada-Nasl-Sper-G-Oldu-inin-ve-inli-Yenilikilerin-rettikleri-e3iqvua"
+    },
     image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1777837876948-2726b92f8b74d.jpg"
   },
   {
@@ -32,6 +44,9 @@ window.PODCAST_FALLBACK_EPISODES = [
     desc: "Bu hafta Otostopçunun Yapay Zeka Rehberi'nde son dönemlerde yaşadığımız, gördüğümüz okuduğumuz gelişmelerle, Yanis Varou...",
     date: "20 Nis 2026",
     link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/TeknoFeodalizm--Palantirin-Teknoloji-Cumhuriyeti-Manifestosu--Sam-Altman-ve-Yapay-Zeka-ile-Gelecek-Bolluk-e3i6lr6",
+    platformLinks: {
+      spotify: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/TeknoFeodalizm--Palantirin-Teknoloji-Cumhuriyeti-Manifestosu--Sam-Altman-ve-Yapay-Zeka-ile-Gelecek-Bolluk-e3i6lr6"
+    },
     image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1776714327083-8e618e6fd4d19.jpg"
   },
   {
@@ -39,6 +54,9 @@ window.PODCAST_FALLBACK_EPISODES = [
     desc: "Otostopçunun Yapay Zeka Rehberi'nde bu bölüm girişimcilik, yatırımcılık, inovasyon ekosistemi tecrübesini kurumsal yapa...",
     date: "10 Nis 2026",
     link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zeka--Giriimcilik--Kurumsal-Dnm-Nisa-Akgn---Dijital--Gelitirme-Yneticisi--Zorlu-Holding-e3hn3ut",
+    platformLinks: {
+      spotify: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zeka--Giriimcilik--Kurumsal-Dnm-Nisa-Akgn---Dijital--Gelitirme-Yneticisi--Zorlu-Holding-e3hn3ut"
+    },
     image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1775827712725-e7b168003950f.jpg"
   },
   {
@@ -46,6 +64,9 @@ window.PODCAST_FALLBACK_EPISODES = [
     desc: "Otostopçunun Yapay Zeka Rehberi'nde bu bölümümüzde, finansın teknolojiyle kırılma noktasını keşfetmek için Garanti BBVA...",
     date: "28 Mar 2026",
     link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zeka--Bankaclk-ve-Finans---Konuk-lker-Kuruz---Garanti-BBVA-Genel-Mdr-Yardmcs-e3h38o5",
+    platformLinks: {
+      spotify: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zeka--Bankaclk-ve-Finans---Konuk-lker-Kuruz---Garanti-BBVA-Genel-Mdr-Yardmcs-e3h38o5"
+    },
     image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1774706723835-d74f740d148aa.jpg"
   },
   {
@@ -53,6 +74,9 @@ window.PODCAST_FALLBACK_EPISODES = [
     desc: "İnsanlık, kendini yeniden yazma gücüne kavuştuğunda ne yapacak? Otostopçunun Yapay Zeka Rehberi'nde bu bölümde DNA'nın k...",
     date: "16 Mar 2026",
     link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zeka-Kansere-are-Bulabilir-Mi--Yapay-Zeka-ve-Biyoteknoloji-e3ghkju",
+    platformLinks: {
+      spotify: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zeka-Kansere-are-Bulabilir-Mi--Yapay-Zeka-ve-Biyoteknoloji-e3ghkju"
+    },
     image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1773686943601-c055f638b92d7.jpg"
   },
   {
@@ -60,6 +84,9 @@ window.PODCAST_FALLBACK_EPISODES = [
     desc: "Otostopçunun Yapay Zeka Rehberi'nin bu bölümünde, yapay zeka dünyasının en sıra dışı şirketlerinden biri olan Anthropic'...",
     date: "8 Mar 2026",
     link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zekann-Etik--Gvenli-irketi--Clauden-Yaratcs-Anthropic-ve-Pentagon-Krizi-e3g3tgq",
+    platformLinks: {
+      spotify: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zekann-Etik--Gvenli-irketi--Clauden-Yaratcs-Anthropic-ve-Pentagon-Krizi-e3g3tgq"
+    },
     image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1772962723468-5e6191b8da4a9.jpg"
   },
   {
@@ -67,6 +94,9 @@ window.PODCAST_FALLBACK_EPISODES = [
     desc: "Otostopçunun Yapay Zeka Rehberi'nde bu hafta The Philosopher in the Valley: Alex Karp, Palantir and the Rise of the Sur...",
     date: "24 Şub 2026",
     link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zeka--Savunma-Sanayii--stihbarat-Hereyin-Palantirlemesi-e3fiaiq",
+    platformLinks: {
+      spotify: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zeka--Savunma-Sanayii--stihbarat-Hereyin-Palantirlemesi-e3fiaiq"
+    },
     image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1771965660640-e1828763cc176.jpg"
   },
   {
@@ -74,6 +104,9 @@ window.PODCAST_FALLBACK_EPISODES = [
     desc: "Otostopçunun Yapay Zeka Rehberi'nin bu bölümünde, yapay zekanın \"kara kutu\" gizemini aralayarak algoritmik kararları...",
     date: "8 Şub 2026",
     link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/50--Blm---Aklanabilir-Yapay-Zeka-Explainable-AI---XAI-e3eptdi",
+    platformLinks: {
+      spotify: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/50--Blm---Aklanabilir-Yapay-Zeka-Explainable-AI---XAI-e3eptdi"
+    },
     image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1770542386100-4e9094aad49d2.jpg"
   },
   {
@@ -81,6 +114,9 @@ window.PODCAST_FALLBACK_EPISODES = [
     desc: "Otostopçunun Yapay Zeka Rehberi'nde bu hafta teknoloji dünyasının yapay zeka odaklı DAVOS notlarına bakacağız beraber...",
     date: "1 Şub 2026",
     link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Teknoloji-Dnyasnn-2026-Davos-ve-Yapay-Zeka-Notlar---Yapay-Zekann-gc-ve-Ekonomi-Etkisi-e3eg3v0",
+    platformLinks: {
+      spotify: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Teknoloji-Dnyasnn-2026-Davos-ve-Yapay-Zeka-Notlar---Yapay-Zekann-gc-ve-Ekonomi-Etkisi-e3eg3v0"
+    },
     image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1769970583544-d466bcab6cd72.jpg"
   },
   {
@@ -88,6 +124,9 @@ window.PODCAST_FALLBACK_EPISODES = [
     desc: "Otostopçunun Yapay Zeka Rehberi'nde bu bölüm, 2000'ler sonrası yapay zekada yaşanan büyük sıçramayı, derin öğrenme...",
     date: "22 Oca 2026",
     link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zeka-Aratrmalar-Bizi-Bu-Gnlere-Nasl-Getirdi-Yeni-Aratrma-a-htiyac-e3e1oij",
+    platformLinks: {
+      spotify: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zeka-Aratrmalar-Bizi-Bu-Gnlere-Nasl-Getirdi-Yeni-Aratrma-a-htiyac-e3e1oij"
+    },
     image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1769104868528-85f525fc81c03.jpg"
   },
   {
@@ -95,6 +134,9 @@ window.PODCAST_FALLBACK_EPISODES = [
     desc: "Otostopçunun Yapay Zeka Rehberi'nde bu bölüm High Agency yani yüksek eylemlilik kavramını derinlemesine ele alıyoruz...",
     date: "17 Oca 2026",
     link: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zeka-ann-Sper-Yetenei-High-Agency-Yksek-Eylemlilik-Nedir--Nasl-Kazanlr-e3dpejd",
+    platformLinks: {
+      spotify: "https://podcasters.spotify.com/pod/show/fatih-bildirici5/episodes/Yapay-Zeka-ann-Sper-Yetenei-High-Agency-Yksek-Eylemlilik-Nedir--Nasl-Kazanlr-e3dpejd"
+    },
     image: "https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_episode/43182597/43182597-1768642113453-067ebf047b98.jpg"
   }
 ];
